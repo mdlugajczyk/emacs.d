f18fdfeff8d329091a251ee3750d95ccd054cf99
Marcin Długajczyk <dlugajczykmarcin@gmail.com>
2015-02-26 18:42:58 +0000

    Disable company mode in gdb.

--- init-cpp.el
+++ init-cpp.el
@@ -29,3 +29,7 @@
 ;; (define-key c++-mode-map  [(control tab)] 'moo-complete)
 ;; (define-key c-mode-map (kbd "M-o")  'fa-show)
 ;; (define-key c++-mode-map (kbd "M-o")  'fa-show)
+
+(add-hook 'gud-gdb-mode-hook
+	  (lambda ()
+	    (company-mode -1)))
