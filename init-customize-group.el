5ea47758f900b28715dc9efe35ee34fcbc2e3da9
Marcin Długajczyk <dlugajczykmarcin@gmail.com>
2015-11-08 11:30:28 +0000

    fds specific customize group.

--- init-customize-group.el
+++ init-customize-group.el
@@ -1,28 +1,30 @@
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(company-preview ((t (:foreground "darkgray" :underline t))))
- '(company-preview-common ((t (:inherit company-preview))))
- '(company-tooltip ((t (:background "lightgray" :foreground "black"))))
- '(company-tooltip-common ((((type x)) (:inherit company-tooltip :weight bold)) (t (:inherit company-tooltip))))
- '(company-tooltip-common-selection ((((type x)) (:inherit company-tooltip-selection :weight bold)) (t (:inherit company-tooltip-selection))))
- '(company-tooltip-selection ((t (:background "steelblue" :foreground "white"))))
- '(ediff-current-diff-A ((t (:background "#553333" :foreground "black"))))
- '(ediff-current-diff-B ((t (:foreground "#335533"))))
- '(fa-face-hint ((t (:background "white" :foreground "black"))))
- '(fa-face-hint-bold ((t (:background "blue" :foreground "black"))))
- '(fa-face-semi ((t (:background "white" :foreground "black" :weight bold))))
- '(fa-face-type ((t (:background "white" :foreground "black"))))
- '(fa-face-type-bold ((t (:background "blue" :foreground "black"))))
- '(magit-diff-added ((t (:foreground "#ddffdd"))))
- '(magit-diff-added-highlight ((t (:background "grey25" :foreground "#cceecc"))))
- '(magit-diff-base ((t (:foreground "#ffffcc"))))
- '(magit-diff-base-highlight ((t (:foreground "#eeeebb"))))
- '(magit-diff-removed ((t (:foreground "#ffdddd"))))
- '(magit-diff-removed-highlight ((t (:background "grey25" :foreground "#eecccc"))))
- '(magit-item-highlight ((t nil))))
+
+(when (fonix-box-p)
+  (custom-set-faces
+   ;; custom-set-faces was added by Custom.
+   ;; If you edit it by hand, you could mess it up, so be careful.
+   ;; Your init file should contain only one such instance.
+   ;; If there is more than one, they won't work right.
+   '(company-preview ((t (:foreground "darkgray" :underline t))))
+   '(company-preview-common ((t (:inherit company-preview))))
+   '(company-tooltip ((t (:background "lightgray" :foreground "black"))))
+   '(company-tooltip-common ((((type x)) (:inherit company-tooltip :weight bold)) (t (:inherit company-tooltip))))
+   '(company-tooltip-common-selection ((((type x)) (:inherit company-tooltip-selection :weight bold)) (t (:inherit company-tooltip-selection))))
+   '(company-tooltip-selection ((t (:background "steelblue" :foreground "white"))))
+   '(ediff-current-diff-A ((t (:background "#553333" :foreground "black"))))
+   '(ediff-current-diff-B ((t (:foreground "#335533"))))
+   '(fa-face-hint ((t (:background "white" :foreground "black"))))
+   '(fa-face-hint-bold ((t (:background "blue" :foreground "black"))))
+   '(fa-face-semi ((t (:background "white" :foreground "black" :weight bold))))
+   '(fa-face-type ((t (:background "white" :foreground "black"))))
+   '(fa-face-type-bold ((t (:background "blue" :foreground "black"))))
+   '(magit-diff-added ((t (:foreground "#ddffdd"))))
+   '(magit-diff-added-highlight ((t (:background "grey25" :foreground "#cceecc"))))
+   '(magit-diff-base ((t (:foreground "#ffffcc"))))
+   '(magit-diff-base-highlight ((t (:foreground "#eeeebb"))))
+   '(magit-diff-removed ((t (:foreground "#ffdddd"))))
+   '(magit-diff-removed-highlight ((t (:background "grey25" :foreground "#eecccc"))))
+   '(magit-item-highlight ((t nil)))))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
