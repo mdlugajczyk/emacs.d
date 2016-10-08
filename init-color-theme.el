7f1d1800e6711a61ce9ce8dfc3d0c81208711927
Marcin Długajczyk <dlugajczykmarcin@gmail.com>
2016-10-08 16:26:01 +0100

    Changing color theme.

--- init-color-theme.el
+++ init-color-theme.el
@@ -1,7 +1,9 @@
 (when window-system
   (maybe-install-package 'color-theme)
-  (maybe-install-package 'zenburn)
+  ;(maybe-install-package 'zenburn)
 
-  (require 'color-theme)
-  (require 'zenburn)
-  (zenburn))
+  ;; ;; (require 'color-theme)
+  ;; ;; (require 'zenburn)
+  ;;  (zenburn)
+  (maybe-install-package 'gruvbox-theme)
+  (load-theme 'gruvbox t))
