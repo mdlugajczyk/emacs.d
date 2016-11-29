ee8a822666c2ba593402be45ef3418d62d97550f
Marcin Dlugajczyk <mdlugajczyk@factset.com>
2016-11-29 05:47:43 -0500

    Remove dead code.

--- init-color-theme.el
+++ init-color-theme.el
@@ -1,9 +1,4 @@
 (when window-system
   (maybe-install-package 'color-theme)
-  ;(maybe-install-package 'zenburn)
-
-  ;; ;; (require 'color-theme)
-  ;; ;; (require 'zenburn)
-  ;;  (zenburn)
   (maybe-install-package 'gruvbox-theme)
   (load-theme 'gruvbox t))
